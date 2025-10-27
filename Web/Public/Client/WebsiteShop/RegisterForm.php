2a5c07cea6b4c77d941b76137eba2fcc73532f02
Kiyoshi-Loc <kiyoshilv123@gmail.com>
2025-10-27 13:39:48 +0700

    fix Index and folder tree

--- Web/Public/Client/WebsiteShop/RegisterForm.php
+++ Web/Public/Client/WebsiteShop/RegisterForm.php
@@ -1,10 +1,10 @@
-<div class="login-container">
+<div class="register-container">
     <div class="logo">
         <a href = "#"><img src="../assets/img/logo.png" alt="Logo" /></a>
     </div>
-    <h2 class="login-title">ĐĂNG NHẬP</h2>
+    <h2 class="register-title">ĐĂNG KÝ</h2>
 
-    <form action="/login-action" method="post" class="login-form">
+    <form action="/login-action" method="post" class="register-form">
         <div class="form-group">
             <input type="text" name="username" placeholder="Số điện thoại / Email" required>
         </div>
@@ -13,10 +13,10 @@
             <input type="password" name="password" placeholder="Mật khẩu" required>
         </div>
 
-        <button type="submit" class="btn-login">ĐĂNG NHẬP</button>
+        <button type="submit" class="btn-register">ĐĂNG NHẬP</button>
 
-        <div class="login-links">
-            <a href="Index.php?do=RegisterForm">Đăng ký</a>
+        <div class="register-links">
+            <a href="Index.php?do=Register">Đăng ký</a>
             <a href="Index.php?do=Forgot_Password">Quên mật khẩu</a>
         </div>
     </form>
