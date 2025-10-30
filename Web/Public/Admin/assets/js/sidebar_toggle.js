661904bd08219a186c499f31098979d8d911a567
Dow <thaihocng204@gmail.com>
2025-10-30 19:51:07 +0700

    merge LTH_fixbug

--- Web/Public/Admin/assets/js/sidebar_toggle.js
+++ Web/Public/Admin/assets/js/sidebar_toggle.js
@@ -1,7 +1,4 @@
 document.querySelectorAll('.menu-toggle').forEach(button => {
-
-
-    
     button.addEventListener('click', () => {
         const item = button.parentElement;
         item.classList.toggle('active');
